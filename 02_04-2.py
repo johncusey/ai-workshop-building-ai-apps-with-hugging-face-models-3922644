fbaf46c18ba7af9127259c2df23d2e79d9ce129f
Dhhyey Desai <57276442+dhhyey-desai@users.noreply.github.com>
2024-10-24 18:35:54 +0100

    Delete 02_04-2.py

--- 02_04-2.py
+++
@@ -1,8 +0,0 @@
-from datasets import load_dataset
-
-dataset = load_dataset("wikipedia", "20220301.en", split="train", streaming=True)
-
-for i, batch in enumerate(dataset):
-    print(batch)
-    if i > 3:
-        break
